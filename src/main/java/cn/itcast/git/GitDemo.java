aaa10f06fe2b180815e501e185d61693e6ec5e13
chenzxin1 <52034799+chenzxin1@users.noreply.github.com>
2019-06-21 15:25:22 +0800

    Update GitDemo.java

--- src/main/java/cn/itcast/git/GitDemo.java
+++ src/main/java/cn/itcast/git/GitDemo.java
@@ -7,5 +7,6 @@ public class GitDemo {
         System.out.println("master modify main!");
         System.out.println("你好,我来啦....");
         System.out.println("你讲的真好...");
+        System.out.println("remote修改");
     }
 }
