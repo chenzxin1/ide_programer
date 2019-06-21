ddbf63316cf29c36e56b5f67a8958e765c42a726
chenzxin <chenzxin@sina.com>
2019-06-21 08:31:36 +0800

    更新项目

--- src/main/java/cn/itcast/git/GitDemo.java
+++ src/main/java/cn/itcast/git/GitDemo.java
@@ -3,5 +3,6 @@ package cn.itcast.git;
 public class GitDemo {
     public static void main(String[] args) {
         System.out.println("---------");
+        System.out.println("^^^^=====^^^^");
     }
 }
