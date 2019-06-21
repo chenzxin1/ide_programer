b8b639d40ddad9d5e2b582ca97e47a885b85928f
chenzxin <chenzxin@sina.com>
2019-06-21 15:06:41 +0800

    dev修改主函数

--- src/main/java/cn/itcast/git/GitDemo.java
+++ src/main/java/cn/itcast/git/GitDemo.java
@@ -4,5 +4,7 @@ public class GitDemo {
     public static void main(String[] args) {
         System.out.println("---------");
         System.out.println("^^^^=====^^^^");
+        System.out.println("你好,我来啦....");
+        System.out.println("你讲的真好...");
     }
 }
