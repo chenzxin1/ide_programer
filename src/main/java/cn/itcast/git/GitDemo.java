c6555201c1304960a49fe10966f221657f4dcedf
chenzxin <chenzxin@sina.com>
2019-06-21 15:09:58 +0800

    master修改主函数

--- src/main/java/cn/itcast/git/GitDemo.java
+++ src/main/java/cn/itcast/git/GitDemo.java
@@ -4,5 +4,6 @@ public class GitDemo {
     public static void main(String[] args) {
         System.out.println("---------");
         System.out.println("^^^^=====^^^^");
+        System.out.println("master modify main!");
     }
 }
